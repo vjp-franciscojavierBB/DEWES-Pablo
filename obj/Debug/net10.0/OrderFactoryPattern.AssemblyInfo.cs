2f0236901059179991675ddddb3b2a8984cf73e0
Javier <fjbecerrob02@informatica.iesvalledeljerteplasencia.es>
2026-09-22 18:40:39 +0200

    Run

--- obj/Debug/net10.0/OrderFactoryPattern.AssemblyInfo.cs
+++ obj/Debug/net10.0/OrderFactoryPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderFactoryPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7704006d1c464ef7ca6efed7111f94857c2e573c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d02aacba32dfd6de38bf9c1bd525b5cde25861")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderFactoryPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderFactoryPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
